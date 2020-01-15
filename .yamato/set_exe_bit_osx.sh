7d91ded3d69801b2a9dbe0efac09d56ca9c3502c
Domenico Villani <domenico.villani@unity3d.com>
2020-01-15 17:03:24 +0100

    legacy files removed [skip ci]

--- .yamato/set_exe_bit_osx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod -v +x Artifacts/Builds/Binaries/Mac/csc
-ls -l Artifacts/Builds/Binaries/Mac/csc
-chmod -v +x Artifacts/Builds/Binaries/Mac/VBCSCompiler
-ls -l Artifacts/Builds/Binaries/Mac/VBCSCompiler
